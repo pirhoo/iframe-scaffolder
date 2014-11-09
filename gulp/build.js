0b4f1ab9690261ca8d8589671c75da53f8174a6a
Pirhoo <hello@pirhoo.com>
2014-11-09 16:13:04 +0100

    Debug images build

--- gulp/build.js
+++ gulp/build.js
@@ -87,11 +87,11 @@ gulp.task('html', ['styles', 'scripts', 'partials'], function () {
 
 gulp.task('images', function () {
   return gulp.src('src/assets/images/**/*')
-    .pipe($.cache($.imagemin({
-      optimizationLevel: 3,
-      progressive: true,
-      interlaced: true
-    })))
+      .pipe($.imagemin({
+        optimizationLevel: 3,
+        progressive: true,
+        interlaced: true
+      }))
     .pipe(gulp.dest('dist/assets/images'))
     .pipe($.size());
 });
