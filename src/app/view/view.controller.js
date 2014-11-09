6cbed183453c572c76e99193de5a910f59651325
Pirhoo <hello@pirhoo.com>
2014-11-09 15:59:28 +0100

    Update 2014-11-09T14:59:26.661Z

--- src/app/view/view.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('iframeScaffolder').controller('ViewCtrl', function ($scope, $stateParams) {
-  $scope.layout = $stateParams.layout
-  $scope.urls = $stateParams.urls.split(",")
-});
--- test/e2e/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-describe('The main view', function () {
-
-  beforeEach(function () {
-    browser.get('http://localhost:3000/index.html');
-  });
-
-});
