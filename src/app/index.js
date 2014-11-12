8a5aab5c07ea7a2ad61d21fead46d739bd5409fb
pirhoo <pierre.romera@gmail.com>
2014-11-12 11:35:10 +0100

    Added google analytcis

--- src/app/index.js
+++ src/app/index.js
@@ -27,4 +27,11 @@ angular.module('iframeScaffolder', ['ngSanitize', 'ui.router', 'zeroclipboard', 
       swfPath: 'bower_components/zeroclipboard/dist/ZeroClipboard.swf'
     });
   })
+  .run(['$rootScope', '$location', '$window', function($rootScope, $location, $window){
+    $rootScope
+      .$on('$stateChangeSuccess', function(){
+        if (!$window.ga) { return; }
+        $window.ga('send', 'pageview', { page: $location.url() });
+      });
+  }])
 ;
