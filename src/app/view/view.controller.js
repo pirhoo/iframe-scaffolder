6a4af4d73b48ceb3a2c900e08e7b871a61ffd2e7
Pirhoo <hello@pirhoo.com>
2015-02-06 10:30:53 +0100

    Added missing theme capture

--- src/app/view/view.controller.js
+++ src/app/view/view.controller.js
@@ -2,5 +2,6 @@
 
 angular.module('iframeScaffolder').controller('ViewCtrl', function ($scope, $stateParams) {
   $scope.layout = $stateParams.layout;
+  $scope.theme  = $stateParams.theme || 'default';
   $scope.urls   = $stateParams.urls.split(',');
 });
