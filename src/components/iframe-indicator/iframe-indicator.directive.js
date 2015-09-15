975c374a16a84946e66b4310f75335cbf04e57e8
pirhoo <hello@pirhoo.com>
2015-09-15 15:27:33 +0200

    Added loader

---
+++ src/components/iframe-indicator/iframe-indicator.directive.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('iframeScaffolder').directive('iframeIndicator', function() {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      // Find the only iframe in the element
+      var iframe = element.find('iframe');
+      // Fired when the iframe is loaded
+      iframe.on('load', function() {
+        // Remove the loading iframe
+        element.removeClass('iframe-indicator--loading');
+      });
+
+      // Watch for change on the iframe URL
+      scope.$watch(function() { return iframe.attr('src'); }, function() {
+        // Add a class to notice that it is loading
+        element.addClass('iframe-indicator--loading');
+      }, true);
+    }
+  };
+});
