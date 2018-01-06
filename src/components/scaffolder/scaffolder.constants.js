bb506bdacaddbe71dbb1312529ac61327b26f689
pirhoo <hello@pirhoo.com>
2018-01-06 18:13:53 +0100

    Added more themes

--- src/components/scaffolder/scaffolder.constants.js
+++ src/components/scaffolder/scaffolder.constants.js
@@ -32,7 +32,18 @@ angular
         { slug: 'picton-blue', label: 'Picton blue' },
         { slug: 'silver-tree', label: 'Silver tree' },
         { slug: 'eucalyptus', label: 'Eucalyptus' },
+        { slug: 'amarantha', label: 'Amarantha' },
         { slug: 'sunset-orange', label: 'Sunset orange' },
-        { slug: 'monza-red', label: 'Monza red' }
+        { slug: 'monza-red', label: 'Monza red' },
+        { slug: 'buddha-gold', label: 'Buddha gold' },
+        { slug: 'ebony-clay-filled', label: 'Filled Ebony clay' },
+        { slug: 'picton-blue-filled', label: 'Filled Picton blue' },
+        { slug: 'silver-tree-filled', label: 'Filled Silver tree' },
+        { slug: 'eucalyptus-filled', label:  'Filled Eucalyptus' },
+        { slug: 'amarantha-filled', label: 'Filled Amarantha' },
+        { slug: 'sunset-orange-filled', label: 'Filled Sunset orange' },
+        { slug: 'monza-red-filled', label: 'Filled Monza red' },
+        { slug: 'buddha-gold-filled', label: 'Filled Buddha gold' },
+        { slug: 'gorse', label: 'Filled Gorse' },
       ]
     });
